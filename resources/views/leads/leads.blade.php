a8c2124538e646e3c84fb5310c2a7a01212a2bd5
rusvlp <qdrwlad@gmail.com>
2023-03-31 13:49:23 +0300

    initial

---
+++ resources/views/leads/leads.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout >
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Все лиды
+        </h2>
+    </x-slot>
+
+    <livewire:leads/>
+</x-app-layout>
